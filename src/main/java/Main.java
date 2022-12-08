3d7cd69145447d24afb600191675562e2728122a
vav74 <111101156+vav74@users.noreply.github.com>
2022-12-08 18:47:17 +0900

    Introduce 'BinOps' HW

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        BinOps bins = new BinOps();
+        System.out.println(bins.sum("10101010", "1010101010"));
+        System.out.println(bins.mult("1010", "101"));
+    }
+}
